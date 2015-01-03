62621d86f85ed826c304089ced34f19f0cc9b3a3
Carlos Peix <carlos.peix@gmail.com>
2015-01-03 14:41:21 -0300

    Problemas del dia 1 resueltos

--- erlang/day-1/self_study.erl
+++ erlang/day-1/self_study.erl
@@ -1,8 +1,11 @@
 -module(self_study).
 -export([count/1]).
 -export([count_to/1]).
+-export([report_result/1]).
+-export([report_result/2]).
 
 %-- Word counter
+%-- Write a function that uses recursion to return the number of words in a string._
 count(Phrase) ->
   internal_count(string:tokens(Phrase, " ")).
 
@@ -13,6 +16,7 @@ internal_count(Words) ->
   1 + internal_count(Tail).
 
 %-- Count to ten
+%-- Write a function that uses recursion to count to ten. 
 count_to(N) -> 
   count_to(1, N).
 
@@ -22,4 +26,13 @@ count_to(Counter, Max) when Counter < Max ->
 
 count_to(_, Max) ->
   io:fwrite("~w~n", [Max]).
+
+%-- Error reporting
+%-- Write a function that uses matching to selectively print "success" or "error: message" given input in the form {error, Message} or success.
+
+report_result(error, Message) ->
+  io:fwrite("~s~n", [Message]).
+
+report_result(success) ->
+  io:fwrite("Success~n").
 
